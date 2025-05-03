7cfbebe67bef5ba598f9b3b0e1099881b4425de8
Harry Dinh <harrydinh1207@gmail.com>
2025-05-03 14:29:47 -0400

    Created project AirRemote

---
+++ AirRemote/AirRemote/AirRemoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirRemoteApp.swift
+//  AirRemote
+//
+//  Created by Harry Dinh on 2025-05-03.
+//
+
+import SwiftUI
+
+@main
+struct AirRemoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
