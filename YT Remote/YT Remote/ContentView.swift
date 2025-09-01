e0dba266324b9800910e041965d415302bc8854d
Harry Dinh <harrydinh1207@gmail.com>
2025-08-31 23:02:27 -0400

    Built a basic UI for testing backend code (#1)

--- YT Remote/YT Remote/ContentView.swift
+++ YT Remote/YT Remote/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tvCode = ""
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            codeTextField
+            navigationButtons
         }
         .padding()
+    }
+
+    private var codeTextField: some View {
+        HStack {
+            TextField("Enter TV Code here...", text: $tvCode)
+                .textFieldStyle(.roundedBorder)
+
+            Button("Connect") {}
+        }
+    }
+
+    private var navigationButtons: some View {
+        Group {
+            Button("UP") {}
+            Button("DOWN") {}
+            Button("LEFT") {}
+            Button("RIGHT") {}
+            Button("SELECT") {}
+        }
     }
 }
 
