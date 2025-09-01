7fa803290ddf963d882e131789cbb725d4d9f638
Harry Dinh <harrydinh1207@gmail.com>
2025-08-31 22:50:19 -0400

    Added a new project for YT Remote

---
+++ YT Remote/YT Remote/YT_RemoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  YT_RemoteApp.swift
+//  YT Remote
+//
+//  Created by Harry Dinh on 2025-08-31.
+//
+
+import SwiftUI
+
+@main
+struct YT_RemoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
