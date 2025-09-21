bbae275791e263446f50fedf2dd92f5d5340f42c
Harry Dinh <harrydinh1207@gmail.com>
2025-09-21 18:25:14 -0400

    Added support for more keys

--- YT Remote/YT Remote Client/KeySignal.swift
+++ YT Remote/YT Remote Client/KeySignal.swift
@@ -12,6 +12,12 @@ enum KeySignal: String {
     case down = "DOWN"
     case left = "LEFT"
     case right = "RIGHT"
+    case `return` = "RETURN"
+    case escape = "ESCAPE"
+    case f8 = "F8"
+    case f10 = "F10"
+    case f11 = "F11"
+    case f12 = "F12"
 }
 
 func pressKey(for signal: KeySignal) {
@@ -21,6 +27,12 @@ func pressKey(for signal: KeySignal) {
         case .down: keyCode = 0x7D
         case .left: keyCode = 0x7B
         case .right: keyCode = 0x7C
+        case .return: keyCode = 0x24
+        case .escape: keyCode = 0x35
+        case .f8: keyCode = 0x64
+        case .f10: keyCode = 0x6D
+        case .f11: keyCode = 0x6B
+        case .f12: keyCode = 0x6F
     }
 
     if let keyDown = CGEvent(keyboardEventSource: nil, virtualKey: keyCode, keyDown: true),
