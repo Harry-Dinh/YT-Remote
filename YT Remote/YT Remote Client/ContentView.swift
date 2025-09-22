4fb676d32dbc14dd9d2a5fe997f500b10d4cbc9c
Harry Dinh <harrydinh1207@gmail.com>
2025-09-21 22:40:50 -0400

    Built a fancy UI for the Mac client app

--- YT Remote/YT Remote Client/ContentView.swift
+++ YT Remote/YT Remote Client/ContentView.swift
@@ -8,14 +8,66 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let instructionsText =
+    """
+    1. Get the IP address of this computer, you can do so with the command below in a terminal session
+    2. Enter the IP address into the YT Remote app on your iPhone or iPad
+    3. Enjoy!
+    """
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            sidebarView
+
+            Divider()
+
+            VStack(alignment: .leading, spacing: 10) {
+                Text("Instructions")
+                    .font(.title3)
+                    .fontWeight(.bold)
+                ipAddressSection
+            }
         }
         .padding()
+    }
+
+    private var ipAddressSection: some View {
+        Group {
+            Text(instructionsText)
+                .padding(.horizontal)
+            terminalCommandBox
+        }
+    }
+
+    private var terminalCommandBox: some View {
+        GroupBox {
+            Text("ipconfig getifaddr en0")
+                .fontDesign(.monospaced)
+        }
+        .padding()
+    }
+
+    private var stopServerButton: some View {
+        Button(action: {}) {
+            Text("Stop Server")
+        }
+    }
+
+    private var openYouTubeTVButton: some View {
+        Button(action: {}) {
+            Text("Launch YouTube TV")
+        }
+    }
+
+    private var sidebarView: some View {
+        VStack {
+            Text("YT Remote Client")
+                .font(.title)
+                .fontWeight(.bold)
+
+            openYouTubeTVButton
+            stopServerButton
+        }
     }
 }
 
--- YT Remote/YT Remote Client/YT_Remote_ClientApp.swift
+++ YT Remote/YT Remote Client/YT_Remote_ClientApp.swift
@@ -12,10 +12,20 @@ struct YT_Remote_ClientApp: App {
     @State private var serverManager = ServerManager()
 
     var body: some Scene {
-        WindowGroup {
+        Window("YT Remote Client", id: "clientApp") {
             ContentView()
                 .onAppear(perform: serverManager.startServer)
                 .onDisappear(perform: serverManager.stopServer)
+        }
+        .windowToolbarStyle(.unified)
+        .windowToolbarLabelStyle(fixed: .titleAndIcon)
+        .commands {
+            CommandMenu("Actions") {
+                Button("Launch YouTube TV") {}
+                    .keyboardShortcut(.return)
+                Button("Stop Server") {}
+                    .keyboardShortcut(".")
+            }
         }
     }
 }
